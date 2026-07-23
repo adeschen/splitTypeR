6c8080dd21dcc01cf8945732c45f9303e9f52d0d
adeschen <adeschen@hotmail.com>
2026-07-22 20:25:03 -0400

    Add plotDemoSamplesDistribution() function

--- R/externalGraphFunctions.R
+++ R/externalGraphFunctions.R
@@ -108,3 +108,119 @@ plotDensityWithUpscalingSamplesBimodal <- function(x, signature,
         col=c(colorAlternative, colorSignature, colorBorderSamples), 
         box.lty=0, lty=c(4,4,1), cex=0.9)
 }
+
+#' @title  Graph both the alternative and the signature distributions 
+#' obtained for a specific signature
+#'
+#' @description TODO
+#' 
+#' @param x a \code{list} of class "splitTypeResults", the output object 
+#' from \code{runSubtyping}function, to be graphed.
+#'
+#' @param signature a \code{character} string representing the signature 
+#' that will be used to create the graph. The signature must 
+#' be present in the object.
+#' 
+#' @param samples a \code{list} of \code{character} string representing 
+#' the names of the samples that will be used to create the graph. The samples 
+#' must be present in the object.
+#'
+#' @param violinColor a \code{character} string representing the color of the 
+#' line for the kernel density distributions in the graph. 
+#' Default: \code{"black"}.
+#' 
+#' @param pointColor a \code{character} string representing the color of the 
+#' dots representing the enrichment scores for the selected samples in 
+#' the graph. Default: \code{"black"}.
+#' 
+#' @param positionJitter a \code{numeric} representing the amount of 
+#' vertical and horizontal jitter added to the position of the points on the 
+#' graph. Default: \code{0.20}.
+#' 
+#' @param alpha a \code{numeric} representing the amount of the opacity of 
+#' the points on the graph. If \code{NA}, the color is completely transparent. 
+#' Default: \code{0.25}.
+#' 
+#' @param seed a \code{integer} that will be used as seed to make the jitter 
+#' reproducible. If \code{NA}, the seed is initialized with a random value. 
+#' Default: \code{NA}.
+#' 
+#' @return a \code{ggplot} object that contains all the enrichment scores 
+#' obtained through the permutation step with the kernel desntiy distributions 
+#' for the selected samples.
+#' 
+#' @examples
+#'
+#' ## Loading signatures
+#' data("signaturesDemo")
+#'
+#' ## Load demo normalized expected counts for 30 patients
+#' data("expNormalCountsDemo")
+#' 
+#' ## Fix seed for reproducibility
+#' set.seed(1221)
+#' 
+#' ## Run classification on the 30 patients using 20 permutations on 75% of 
+#' ## the dataset, and 10 points per patient for the up-scaling step
+#' results <- runSubtypingBimodal(geneLists=signaturesDemo, 
+#'     expectedCountsMatrix=expNormalCountsDemo, 
+#'     permRatio=0.75, permNbr=30, upscaleNbr=5)
+#'     
+#' ## Graph the enrichment results from the permutation for 3 samples
+#' plotDemoSamplesDistribution(x=results, 
+#'     signature="2018_Tiriac_PDAC_PDO_basal-like_signature",
+#'     samples=c("Patient_9", "Patient_25", "Patient_29"), 
+#'     violinColor="darkred", pointColor="darkviolet", positionJitter=0.20, 
+#'     seed=121)
+#'
+#' @author Astrid Deschênes
+#' @encoding UTF-8
+#' @importFrom stringr str_replace_all
+#' @importFrom rlang .data
+#' @importFrom ggplot2 ggplot aes ggtitle geom_violin element_text theme_minimal position_jitter geom_jitter ylab ylim theme
+#' @export
+plotDemoSamplesDistribution <- function(x, signature, samples, 
+    violinColor="black", pointColor="black", positionJitter=0.20, alpha=0.25, 
+    seed=NA) {
+
+    if (!inherits(x, "splitTypeResults")) {
+        stop("The x object must be of class \'splitTypeResults\'.")
+    }
+
+    if (!signature %in% names(x$PERMUTATIONS)) {
+        stop("The signature \'", signature, "\' must be present in the ", 
+                "\'splitTypeResults\' object.")
+    }
+    
+    if (is.array(samples) && is.character(samples) && length(samples) > 0 && 
+            all(samples %in% rownames(x$PERMUTATIONS[[signatures]]))) {
+        stop("The samples must all be present in the ", 
+                "\'splitTypeResults\' object.")
+    }
+
+    permutRes <- x$PERMUTATIONS[[signature]]
+    nPerm <- ncol(permutRes)
+    nSamples <- length(samples)
+
+    allRes <- list()
+    for (i in samples) {
+        tmp <- data.frame(sample=rep(i, nPerm), score=permutRes[i, ])
+        allRes[[length(allRes) + 1]] <- tmp
+    }
+    allRes <- do.call(rbind, allRes)
+    allRes <- allRes[which(!is.na(allRes$score)),]
+
+    g1 <- ggplot(allRes, aes(x=.data$sample, y=.data$score)) + 
+            geom_violin(color=violinColor, trim=FALSE) + 
+            ggtitle(str_replace_all(signature, "_", " ")) +
+            geom_jitter(shape=16, color=pointColor, alpha=alpha, cex=0.95, 
+                            position=position_jitter(positionJitter, 
+                                                        seed=seed)) +
+            ylab("Enrichment score") + theme_minimal() + 
+            ylim(-1, 1) + xlab("") +
+            theme(axis.text = element_text(size=10),
+                axis.title = element_text(face="bold", size=11),
+                plot.title = element_text(size=12, hjust = 0.5))
+
+    return(g1)
+}
--- tests/testthat/test-externalGraphFunctions.R
+++ tests/testthat/test-externalGraphFunctions.R
@@ -48,7 +48,7 @@ test_that("plotDensityWithUpscalingSamplesBimodal() must return an error when x 
         permRatio=0.75, permNbr=5, upscaleNbr=3)
     
     attr(results, "mode") <- "TEST"
-    
+
     expect_error(plotDensityWithUpscalingSamplesBimodal(x=results, 
         signature="2018_Tiriac_PDAC_PDO_basal-like_signature"), error_message)
 })
@@ -63,4 +63,35 @@ test_that("plotDensityWithUpscalingSamplesBimodal() must return a list when all 
     
     expect_true(is.list(plotDensityWithUpscalingSamplesBimodal(x=results, 
         signature="2018_Tiriac_PDAC_PDO_basal-like_signature")))
+})
+
+#############################################################################
+### Tests plotDemoSamplesDistribution() 
+#############################################################################
+
+test_that("plotDemoSamplesDistribution() must return an error when x is a list", {
+    
+    error_message <- "The x object must be of class \'splitTypeResults\'."
+    
+    gList <- list()
+    gList[["test"]] <- c("ABC1", "ABC2")
+    
+    expect_error(plotDemoSamplesDistribution(x=gList, 
+        signature="test", samples=c("sample_1", "sample_2")), error_message)
+})
+
+test_that("plotDemoSamplesDistribution() must return an error when the signature is not in the object", {
+    
+    error_message <- paste0("The signature \'DO_NOT_TEST\' must be present ", 
+        "in the 'splitTypeResults\' object.")
+    
+    set.seed(1221)
+
+    results <- runSubtypingBimodal(geneLists=signaturesDemo, 
+        expectedCountsMatrix=expNormalCountsDemo, 
+        permRatio=0.75, permNbr=10, upscaleNbr=5)
+    
+    expect_error(plotDemoSamplesDistribution(x=results, 
+        signature="DO_NOT_TEST", samples=c("sample_1", "sample_2")), 
+        error_message)
 })
